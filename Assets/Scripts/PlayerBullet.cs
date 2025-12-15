b99d0a2267ddd81bb41a7f1b8cb9fbec1a006fd5
Emanuele <emabosco211@gmail.com>
2025-12-15 14:56:05 +0100

    fixes

--- Assets/Scripts/PlayerBullet.cs
+++ Assets/Scripts/PlayerBullet.cs
@@ -4,18 +4,23 @@ public class PlayerBullet : MonoBehaviour
 {
     public float speed = 12f;
     public int damage = 1;
-
     Vector2 dir;
 
     public void Init(Vector2 direction)
     {
         dir = direction.normalized;
+        
+        // Ruota il proiettile nella direzione di movimento
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        
         Destroy(gameObject, 3f);
     }
 
     void Update()
     {
-        transform.Translate(dir * speed * Time.deltaTime);
+        // Usa transform.right invece di dir perché il proiettile è già ruotato
+        transform.position += (Vector3)(dir * speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D col)
